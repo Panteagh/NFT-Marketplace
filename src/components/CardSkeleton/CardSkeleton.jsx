3f34ca282ffcffa156b7f8cac05a086426732669
Pantea ghaffari <panteagh.03@gmail.com>
2025-01-03 16:28:13 +0330

    ...

--- src/components/CardSkeleton/CardSkeleton.jsx
+++ src/components/CardSkeleton/CardSkeleton.jsx
@@ -2,59 +2,59 @@ import React from "react";
 
 function CardSkeleton() {
   return (
-    <div className="grid md:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 mx-auto gap-x-6 mt-6">
-      <div class="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
-        <div class="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
-        <div class="flex flex-col gap-2">
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+    <div classNameName="grid md:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 mx-auto gap-x-6 mt-6">
+      <div className="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
+        <div className="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
+        <div className="flex flex-col gap-2">
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
         </div>
       </div>
-      <div class="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
-        <div class="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
-        <div class="flex flex-col gap-2">
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+      <div className="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
+        <div className="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
+        <div className="flex flex-col gap-2">
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
         </div>
       </div>
-      <div class="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
-        <div class="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
-        <div class="flex flex-col gap-2">
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+      <div className="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
+        <div className="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
+        <div className="flex flex-col gap-2">
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
         </div>
       </div>
-      <div class="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
-        <div class="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
-        <div class="flex flex-col gap-2">
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+      <div className="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
+        <div className="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
+        <div className="flex flex-col gap-2">
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
         </div>
       </div>
-      <div class="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
-        <div class="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
-        <div class="flex flex-col gap-2">
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+      <div className="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
+        <div className="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
+        <div className="flex flex-col gap-2">
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
         </div>
       </div>
-      <div class="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
-        <div class="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
-        <div class="flex flex-col gap-2">
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
-          <div class="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
+      <div className="flex flex-col bg-neutral-700 w-56 h-64 animate-pulse rounded-xl p-4 gap-4 mt-4 lg:w-[330px] lg:h-[469px]">
+        <div className="bg-neutral-400/50 w-full h-64 animate-pulse rounded-md"></div>
+        <div className="flex flex-col gap-2">
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-4/5 h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-full h-4 animate-pulse rounded-md"></div>
+          <div className="bg-neutral-400/50 w-2/4 h-4 animate-pulse rounded-md"></div>
         </div>
       </div>
     </div>
--- src/components/ListSkeleton/ListSkeleton.jsx
+++ src/components/ListSkeleton/ListSkeleton.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function ListSkeleton() {
   return (
     <>
-      <div class="animate-pulse flex flex-col items-center gap-4 w-full mt-4">
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
-        <div class="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+      <div className="animate-pulse flex flex-col items-center gap-4 w-full mt-4">
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
+        <div className="h-16 bg-[#3B3B3B] w-full rounded-3xl"></div>
       </div>
     </>
   );
